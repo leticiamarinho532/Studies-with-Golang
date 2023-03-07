f3db616aa06e0cb84fc2e9eef6abebf4206708d5
Leticia <leticia.marinho532@gmail.com>
2023-03-06 23:32:59 -0300

    feat:more explanation of pointers

--- main.go
+++ main.go
@@ -19,6 +19,12 @@ func (o *Order) setPrice(price float64) {
 	fmt.Println("Price dentro do setPrice: ", o.Price)
 }
 
+//constutor em go (go não tem classes)
+// É muito comum utilizar assim, quando cria algo apontando na memoria economiza memoria e evita duplicidade.
+func newOrder () *Order {
+	return &Order{}
+}
+
 func main() {
 	a := 1
 	b := "b"
@@ -30,12 +36,21 @@ func main() {
 	// variavel e esta apontando para o endereco de memoria de d, sendo uma referencia de memoria
 	e := &d
 
-	order := Order{
-		ID: "123",
-		Price: 10.0,
-		Quantity: 5,
-	}
+	// order := Order{
+	// 	ID: "123",
+	// 	Price: 10.0,
+	// 	Quantity: 5,
+	// }
+	order := newOrder()
+	order.ID = "123"
+	order.Quantity = 10
+	order.Price = 10.0
 	order.setPrice(20.0)
 
-	fmt.Println(a, b, c, order, order.getTotal(), d, e, *e)
+	order2 := order
+	order2.Price = 5
+
+
+
+	fmt.Println(a, b, c, order, order.getTotal(), d, e, *e, order2.Price)
 }
